9705a2e680ff858235b862a2192eae955087d5cd
Brian Wangila <bwanjalah@gmail.com>
2022-06-03 15:31:08 +0300

    Done.

---
+++ index.js
@@ -0,0 +1,47 @@
+const userInfo = {
+  firstName: "Avi",
+  lastName: "Flombaum",
+  company: {
+    name: "Flatbook Labs",
+    jobTitle: "Developer Apprentice",
+  },
+  friends: [
+    {
+      firstName: "Nancy",
+      lastName: "Burgess",
+      company: {
+        name: "Flatbook Labs",
+        jobTitle: "Developer Apprentice",
+      },
+    },
+    {
+      firstName: "Corinna",
+      lastName: "Jackson",
+      company: {
+        name: "Flatbook Labs",
+        jobTitle: "Lead Developer",
+      },
+    },
+  ],
+  projects: [
+    {
+      title: "Flatbook",
+      description:
+        "The premier Flatiron School-based social network in the world.",
+    },
+    {
+      title: "Scuber",
+      description:
+        "A burgeoning startup helping busy parents transport their children to and from all of their activities on scooters.",
+    },
+  ],
+};
+
+//iterating over elements and geting values in objects
+
+console.log(userInfo.lastName)
+console.log(userInfo.company.jobTitle)
+
+console.log(userInfo.friends[0].firstName)  //inside the list of friends
+
+console.log(userInfo.projects[1].title)
